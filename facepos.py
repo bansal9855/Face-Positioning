f5b519446192e9a51b5ae2bb5538b2f7f17183d3
Lovely Bansal <bansallovely179@gmail.com>
2024-08-13 15:07:22 +0530

    2nd commit

--- facepos.py
+++ facepos.py
@@ -1,12 +1,14 @@
 from PIL import Image
+from dotenv import load_dotenv
 from landingai.predict import Predictor
 import os
 
 endpoint_id = "57c75c5c-d5e7-4fc5-8b61-2b1709b589de"
-api_key = "land_sk_ovwfuMDU7GUV2xXYLVvACUpCpkNdLkuJlvPioPlm2AuIouyR5y"
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 predictor = Predictor(endpoint_id,api_key=api_key)
-image_dir = "./Face Position/images"
+image_dir = "./images"
 image_files = [f for f in os.listdir(image_dir) if f.endswith(('.jpeg','.jpg','.png'))]
 
 for image_file in image_files[:50]:
